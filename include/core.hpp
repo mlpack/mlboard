77eb2d7324fb39e21fdf65fa34a58a6232324652
jeffin143 <jeffinsam@karunya.edu.in>
2020-06-08 13:04:56 +0530

    setup cmake

---
+++ include/core.hpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <utility>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <thread>
+#include <mutex>
+#include <condition_variable> // NOLINT
+#include <fstream>
+#include <queue>
+#include <chrono>
+#include <ctime>
+#include <stdio.h>
+#include <cstdint>
+#include <cstring>
+#include <future> // NOLINT
