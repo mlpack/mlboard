47d7f734dcc48644386164cf61b611a1c282297e
jeffin143 <jeffinsam@karunya.edu.in>
2020-06-19 19:00:12 +0530

    This time it really worked

--- tests/mock_test.cpp
+++ tests/mock_test.cpp
@@ -2,12 +2,14 @@
  * @file mock_test.cpp
  */
 #include "catch.hpp"
-
+#include "mlboard.hpp"
 
 /**
  * Test the MOCK.
  */
 TEST_CASE("MOCK_TEST", "MOCK")
 {
+    mlboard::FileWriter f1("jeffin");
+    f1.close(); 
     REQUIRE(1 <= 2);
 }
