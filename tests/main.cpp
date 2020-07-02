dee4681de425494d2d35e16d21f314abcd0fff0b
jeffin sam <jeffinsam@karunya.edu.in>
2020-07-03 00:13:09 +0530

    Try to CI setup for Windows  (#9)
    * Add CI for windows
    
    * use new version of catch and apply suggestion from review
    
    * Apply final suggestions from code review
    
    Co-authored-by: Toshal Agrawal <37237262+walragatver@users.noreply.github.com>
    
    Co-authored-by: Toshal Agrawal <37237262+walragatver@users.noreply.github.com>

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,12 +5,17 @@
 
 #include <iostream>
 #include <mlboard/core.hpp>
-#include <unistd.h>
 #include <dirent.h>
 
 //#define CATCH_CONFIG_MAIN  // catch.hpp will define main().
 #define CATCH_CONFIG_RUNNER  // We will define main().
 #include "catch.hpp"
+
+// For windows rmdir.
+#ifdef _WIN32
+  #include <io.h>
+  #include <direct.h>
+#endif
 
 void removeTempDirs()
 {
@@ -28,7 +33,12 @@ void removeTempDirs()
     if (entry->d_name != "." && entry->d_name != ".."
       && std::regex_match(entry->d_name, regExp) == true)
     {
-      int status = rmdir(entry->d_name);
+      int status; 
+      #if defined(_WIN32)
+        status = _rmdir(entry->d_name);
+      #else
+        status = rmdir(entry->d_name);
+      #endif
       if (status == -1)
       {
         std::cout << "Error while removing temp directory: " << entry->d_name
