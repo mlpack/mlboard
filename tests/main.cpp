3afbb074cbc28557fdc6e8c4c7c33734466d06a3
jeffin143 <jeffinsam@karunya.edu.in>
2020-07-10 00:06:30 +0530

    Testing not possible while writing

--- tests/main.cpp
+++ tests/main.cpp
@@ -51,16 +51,14 @@ void removeTempDirs()
 
 int main(int argc, char** argv)
 {
+  #if defined(_WIN32)
+      _mkdir("_templogs");
+  #else 
+      mkdir("_templogs",0777);
+  #endif
+
   std::cout << "Start testing " << std::endl;
 
-    // Create temp dirs.
-    #if defined(_WIN32)
-        _mkdir("_templogs");
-        _mkdir("_templogs");
-    #else 
-        mkdir("_templogs", 0777);
-        mkdir("_templogs", 0777);
-    #endif
 
   int testStatus = Catch::Session().run(argc, argv);
 
--- tests/summarywriter_test.cpp
+++ tests/summarywriter_test.cpp
@@ -13,85 +13,68 @@
     #include <direct.h>
 #endif
 
+class SummaryWriterTestsFixture {
+  public:
+   static mlboard::FileWriter* f1;
+	 static size_t currentSize;
+ };
 
-mlboard::FileWriter f1("_templogs");
-size_t currentSize = 0;
-
-/**
- * Test the scaler summary.
- */
-TEST_CASE("Writing a scaler summary to file", "[SummaryWriter]")
-{	
-  struct stat results;
-
-  for (int i = 1; i < 25; i++)
-  {
-		mlboard::SummaryWriter<mlboard::FileWriter>::Scalar("scalerSample_1",
-		i, 1.1, f1);
-  }  
-
-  stat(f1.FileName().c_str(), &results);
-
-  REQUIRE(results.st_size > currentSize);
-	currentSize += results.st_size;
-  // Remove event files.
-
-}
+mlboard::FileWriter* SummaryWriterTestsFixture::f1;
+size_t SummaryWriterTestsFixture::currentSize = 0;
 
 /**
  * Test the Image summary.
  */
-TEST_CASE( "Writing a Image summary to file", "[SummaryWriter]")
+TEST_CASE_METHOD(SummaryWriterTestsFixture, "Writing a Image summary to file", "[SummaryWriter]")
 {	
-  struct stat results;
+	f1 = new mlboard::FileWriter("_templogs");
+	std::ifstream fin("./data/multiple_image.jpg", std::ios::binary);
+	std::ostringstream ss;
 
-    std::ifstream fin("./data/single_image.jpg", std::ios::binary);
-    std::ostringstream ss;
+	ss << fin.rdbuf();
+	std::string image(ss.str());
+	fin.close();
+	mlboard::SummaryWriter<mlboard::FileWriter>::Image(
+				"Test Image", 1, image, 512, 512, 3, *f1, "Sample Image",
+				"This is a Sample image logged using mlboard.");
+}
 
-    ss << fin.rdbuf();
-    std::string image(ss.str());
-    fin.close();
-    std::cout << "Length of string image "<<image.length() << "\n";
-		mlboard::SummaryWriter<mlboard::FileWriter>::Image(
-         "Test Image", 1, image, 512, 512, 3, f1, "Sample Image",
-         "This is a Sample image logged using mlboard.");
+/**
+ * Test the scaler summary.
+ */
+TEST_CASE_METHOD(SummaryWriterTestsFixture, "Writing a scaler summary to file", "[SummaryWriter]")
+{	
+  // struct stat results;
 
-
-  stat(f1.FileName().c_str(), &results);
-
-  REQUIRE(results.st_size > currentSize);
-	currentSize += results.st_size;
+  for (int i = 1; i < 25; i++)
+  {
+		mlboard::SummaryWriter<mlboard::FileWriter>::Scalar("scalerSample_1",
+		i, 1.1, *f1);
+  }  
 }
 
 /**
  * Test multiple Image summary.
  */
-TEST_CASE("Writing multiple Images summary to file", "[SummaryWriter]")
+TEST_CASE_METHOD(SummaryWriterTestsFixture, "Writing multiple Images summary to file", "[SummaryWriter]")
 {	
-  struct stat results;
+	arma::Mat<unsigned char> matrix;
+	mlpack::data::ImageInfo info;
+	std::vector<std::string> files = {"./data/single_image.jpg",
+				"./data/single_image.jpg"};
 
-    arma::Mat<unsigned char> matrix;
-    mlpack::data::ImageInfo info;
-    std::vector<std::string> files = {"data/single_image.jpg",
-         "data/single_image.jpg", "data/single_image.jpg",
-				 "data/single_image.jpg", "data/single_image.jpg"};
+	// Creating the matrix which has image.
+	mlpack::data::Load(files, matrix, info, false);
 
-    // Creating the matrix which has image.
-    mlpack::data::Load(files, matrix, info, false);
+	// Now we can log the matrix.
+	mlboard::SummaryWriter<mlboard::FileWriter>::Image(
+				"Multiple Image", 1, matrix, info, *f1, "Sample Multiple Image",
+				"This is a Sample multiple image logged using mlboard.");
 
-    // Now we can log the matrix.
-    mlboard::SummaryWriter<mlboard::FileWriter>::Image(
-         "Multiple Image", 1, matrix, info, f1, "Sample Multiple Image",
-         "This is a Sample image logged using mlboard.");
-
-  stat(f1.FileName().c_str(), &results);
-
-  REQUIRE(results.st_size > currentSize);
-	currentSize += results.st_size;
+	f1->Close();
 
 	#ifndef KEEP_LOGS
-		std::cout<<"removing file \n";
-		// remove(f1.FileName().c_str());
+		remove(f1.FileName().c_str());
 	#endif
 
 }
